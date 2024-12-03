d299222f577a7765d4784c84a29f939318c3369c
jo-it-solution <yosishimelis@gmail.com>
2024-12-03 15:30:18 +0300

    first commit

---
+++ myport/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Myport from './my';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <Myport/>
+    </div>
+  );
+}
+
+export default App;
